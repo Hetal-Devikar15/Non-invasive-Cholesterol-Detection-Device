18e48f5f313328d3f72cfb6c546aa61d22df15aa
xncoder <131364461+xn-coder@users.noreply.github.com>
2025-01-02 23:28:54 +0530

    Update urls.py

--- frontend/cholesterol/urls.py
+++ frontend/cholesterol/urls.py
@@ -5,6 +5,4 @@ from btapp import views
 urlpatterns = [
     path('admin/', admin.site.urls),
     path("", views.index, name="index"),
-    path("qr/", views.qr_code, name="qr"),
-    path("data/", views.sse_view, name="sse"),
 ]
